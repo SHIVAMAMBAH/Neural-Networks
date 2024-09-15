ba2964b12abd71cfcc1a6331617e007c4ede2e0b
SDiHALF <shivamsharma22468@gmail.com>
2024-09-15 14:13:32 +0530

    Update main.cpp

--- Perceptron models/Identifying a Number in an Array/main.cpp
+++ Perceptron models/Identifying a Number in an Array/main.cpp
@@ -2,7 +2,7 @@
 #include<vector>
 #include<cmath>
 
-#define LEARNINGRATE 0.01
+#define LEARNINGRATE 0.01 // lower learning rate for better stability
 #define TOLERANCE 0.000001
 #define maxEpoch 1000
 
@@ -37,13 +37,13 @@ std::vector<double> WeightedSum(std::vector<std::vector<double>> &Dataset,std::v
 double ReLU(double x){
     return x>=0?x:0;
 }
-
+// backpropagation algorithm
 void Backpropogation(std::vector<std::vector<double>> &Dataset,std::vector<double> &targets,std::vector<double> &weights,size_t maxEpoch){
     
     size_t rows  = numberOfRows(Dataset);
     size_t cols = numberOfColumns(Dataset);
     
-    double previousLoss = std::numeric_limits<double>::max();
+    double previousLoss = std::numeric_limits<double>::max();// Initialize to a large value
     
     for(size_t epoch = 0;epoch<maxEpoch;epoch++){
         std::vector<double> myvec = WeightedSum(Dataset,weights);
@@ -58,7 +58,7 @@ void Backpropogation(std::vector<std::vector<double>> &Dataset,std::vector<doubl
             totalloss+=std::pow(error,2);
         }
         if(std::fabs(previousLoss - totalloss)<TOLERANCE){
-            break;
+            break; // stop training if the loss improvement is below tolerance
         }
         previousLoss = totalloss;
     }
@@ -66,20 +66,20 @@ void Backpropogation(std::vector<std::vector<double>> &Dataset,std::vector<doubl
 
 int main()
 {
-    
+    // dataset vector, each rows acts as an independent dataset
     std::vector<std::vector<double>> Dataset = {{1,0,0,0},
                                                 {0,1,0,0},
                                                 {0,0,1,0},
                                                 {0,0,0,1}};
     
-    std::vector<double> weights = {-0.4,0.3,-0.1,0.7};
-    std::vector<double> targets = {1,1,1,1};
+    std::vector<double> weights = {-0.4,0.3,-0.1,0.7}; // weights initialised randomly
+    std::vector<double> targets = {1,1,1,1}; // target output for each flattened row
     
     Backpropogation(Dataset,targets,weights,maxEpoch);
     std::cout<<"New Weight Vector"<<"\n";
+    // printing the new updated weights
    for(double w:weights){
         std::cout<<w<<" ";
     }
-    
     return 0;
 }
